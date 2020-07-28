f6ae861bc92c3e6b186aff11407efcdfb4f33b39
Chance Rutledge <chancerutledge2@gmail.com>
2020-07-28 10:20:21 -0700

    basic logic implemented

--- conwaygameoflife/src/App.js
+++ conwaygameoflife/src/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, setTimeout, useRef } from "react";
 import produce from "immer";
 // import "./App.css";
 
 function App() {
   const numRows = 50;
   const numCols = 50;
+
+  const operations = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0],
+  ];
 
   const [grid, setGrid] = useState(() => {
     const rows = [];
@@ -14,10 +25,55 @@ function App() {
     return rows;
   });
   // console.log(grid);
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!running.current) {
+      return;
+    }
+    //simulate
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(runSimulation, 1000);
+  }, []);
 
   return (
     <>
-      <button> Start </button>
+      <button
+        onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            runSimulation();
+          }
+        }}
+      >
+        {running ? "Stop" : "Start"}
+      </button>
       <div
         style={{
           display: "grid",
@@ -47,23 +103,5 @@ function App() {
     </>
   );
 }
-
-//   return(
-//   <div>
-//     {grid.map((rows, i) =>
-//       rows.map((col, k) => (
-//         <div
-//           key={`${i}-{k}`}
-//           style={{
-//             width: 20,
-//             height: 20,
-//             backgroundColor: grid[i][k] ? "black" : undefined,
-//             border: "solid 1px black",
-//           }}
-//         />
-//       ))
-//     )}
-//   </div>
-// )
 
 export default App;
